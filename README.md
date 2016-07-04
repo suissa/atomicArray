1f8dab89eb4d6396051f5a0dbcb6e9377e41b5ca
Suissa <jnascimento@gmail.com>
2016-07-04 00:36:36 -0300

    Refactored

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
-# Random Array
+# Atomic Array
 
 ```js
-atomicArray(length, functionToCreateTheValue)
+const atomicArray = require('atomic-array')
+atomicArray.create(length, functionToCreateTheValue)
 ```
 
 For example lets create an Array with 10 elements filling with value 0:
 
 ```js
-atomicArray(10, () => 0)
+atomicArray.create(10, () => 0)
 // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
 ```
 
 And how it´s a create function expected we can easily create random numbers using external modules like: [random-numbers](https://www.npmjs.com/package/random-numbers), [random-even-numbers](https://www.npmjs.com/package/random-even-numbers), [random-odd-numbers](https://www.npmjs.com/package/random-odd-numbers), etc.
 
 ```js
-'use strict'
+const atomicArray = require('atomic-array')
+const randomNumbers= require('random-numbers')
 
-const create = {}
-create._Array = require('atomic-array')
-create._Number = require('random-numbers')
-
-const arr = create._Array.atomicArray(10, create._Number.randomNumber)
-console.log('arr', arr)
+const arr = atomicArray.create(10, randomNumbers.create)
 // [ 88, 23, 55, 13, 4, 99, 16, 72, 81, 2 ]
 ```
--- atomicArray.test.js
+++ atomicArray.test.js
@@ -1,12 +1,12 @@
 const expect = require('chai').expect
-const create = require('./atomicArray')
+const atomicArray = require('./atomicArray')
 
 describe('atomicArray', () => {  
   const testNameTruey = 'a create function is needed!'
   describe(testNameTruey,  () => {
     it('testing if '+testNameTruey, () => {
       try {
-        create.atomicArray(10)
+        atomicArray.create(10)
       } catch(e) {
         expect(e).to.be.an('error')
       }
@@ -15,13 +15,13 @@ describe('atomicArray', () => {
   const testNameLength = 'the length of returned Array'
   describe(testNameLength,  () => {
     it('testing if '+testNameLength+' is equal to 10', () => {
-      expect(create.atomicArray(10, ()=>0).length).to.equal(10)
+      expect(atomicArray.create(10, ()=>0).length).to.equal(10)
     })
   })
   const testNameValues = 'the values of returned Array'
   describe(testNameValues,  () => {
     it('testing if '+testNameValues+' is equal to 0', () => {
-      const _arr = create.atomicArray(10, ()=>0)
+      const _arr = atomicArray.create(10, ()=>0)
       _arr.forEach( (element, index) => {
         expect(element).to.equal(0)
       })
